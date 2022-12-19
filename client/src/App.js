67ead9681dd8f3afe58a4b3021b8c3bc690f8d2b
ayayousef96 <ayayou1205@gmail.com>
2022-12-19 09:28:06 +0200

    client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  const [amount, setAmount] = useState("");
+  const [transfer, setTransfer] = useState("");
+  const [userId, setUserId] = useState("");
+  const [passportId, setPassportId] = useState("");
+  
+  return (
+   
+  );
+}
+
+export default App;
